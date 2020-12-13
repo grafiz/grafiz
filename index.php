5ca6c527b78508889a452825d732e88c0a436465
grafiz <grafiz.creation@gmail.com>
2020-12-13 08:48:41 +0100

    modification fichier index.php

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
-require 'config.php';
-ob_start("minifier"); 
-function minifier($code) { 
-    $search = array( 
-          
-        // Remove whitespaces after tags 
-        '/\>[^\S ]+/s', 
-          
-        // Remove whitespaces before tags 
-        '/[^\S ]+\</s', 
-          
-        // Remove multiple whitespace sequences 
-        '/(\s)+/s', 
-          
-        // Removes comments 
-        '/<!--(.|\s)*?-->/'
-    ); 
-    $replace = array('>', '<', '\\1'); 
-    $code = preg_replace($search, $replace, $code); 
-    return $code; 
-}
 use App\Router;
-
-
+require 'config.php';
 
 // je charge "autoload" pour charger dynamiquement la classe que l'on souhaite.
 require ROOT . "/vendor/autoload.php";
